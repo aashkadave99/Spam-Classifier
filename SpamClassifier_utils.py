5a60d48613eaa61c9717f248654a2ec4ec283d9d
Aashka Dave <aashka.dave99@gmail.com>
2021-04-29 23:32:13 +0530

    Create full matrix for training

--- SpamClassifier_utils.py
+++ SpamClassifier_utils.py
@@ -12,6 +12,7 @@ import numpy as np
 #generator function
 
 def email_body_generator(path):
+    
     for root, dirnames, filenames in walk(path):
         for file_name in filenames:
             filepath = join(root,file_name)
@@ -26,15 +27,18 @@ def email_body_generator(path):
                     is_body = True
             stream.close()
             email_body = '\n'.join(lines)
+            
             yield file_name, email_body
 
 def df_from_directory(path, classification):
+    
     rows=[]
     row_names=[]
 
     for file_name, email_body in email_body_generator(path):
         rows.append({'MESSAGE': email_body,'CATEGORY': classification})
         row_names.append(file_name)
+        
     return pd.DataFrame(rows, index=row_names)
 
 def clean_msg(message, stemmer= PorterStemmer(), stop_words=set(stopwords.words('english'))):
@@ -76,3 +80,31 @@ def sparse_matrix(df, index_words, labels):
 
     return pd.DataFrame(dictList) 
 
+def make_full_matrix(sparse_matrix, nr_words, doc_idx=0, word_idx=1, cat_idx=2, freq_idx =3,):
+    '''
+    Form a full matrix from a sparse matrix. Return panda dataframe
+    sparse_matrix -- numpy array
+    nr_words -- size of vocabulary, total number of tokens
+    doc_idx -- position of document id in sparse matrix default: 1st col
+    word -- position of word id in sparse matrix default: 2nd col
+    cat_idx -- position of label in sparse matrix default: 3rd col
+    freq_idx -- position of occurence of word in sparse matrix default: 4th col
+    '''
+    col_names = ['DOC_IDS'] + ['CATEGORY'] + list(range(0,nr_words))
+    doc_id_names = np.unique(sparse_matrix[:,0])
+    full_matrix = pd.DataFrame(index= doc_id_names, columns= col_names).fillna(value=0)
+    
+    for i in range(sparse_matrix.shape[0]):
+        
+        doc_nr = sparse_matrix[i][doc_idx]
+        word_id = sparse_matrix[i][word_idx]
+        label = sparse_matrix[i][cat_idx]
+        occurence = sparse_matrix[i][freq_idx]
+        
+        full_matrix.at[doc_nr, 'DOC_ID']= doc_nr
+        full_matrix.at[doc_nr, 'CATEGORY']= label
+        full_matrix.at[doc_nr, word_id]= occurence
+        
+    full_matrix.set_index('DOC_ID', inplace=True)
+    
+    return full_matrix
